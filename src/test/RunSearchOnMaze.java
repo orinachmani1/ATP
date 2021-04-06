109c166e4b9503707c4e781df8300de92d9e4bd3
orinachmani1 <73288898+orinachmani1@users.noreply.github.com>
2021-04-06 18:01:36 +0300

    addition of - test package and organized the - packages hirarchi

---
+++ src/test/RunSearchOnMaze.java
@@ -0,0 +1,36 @@
+package test;
+
+import algorithms.mazeGenerators.IMazeGenerator;
+import algorithms.mazeGenerators.Maze;
+import algorithms.mazeGenerators.MyMazeGenerator;
+import algorithms.search.*;
+
+import java.util.ArrayList;
+
+public class RunSearchOnMaze {
+
+    public static void main(String[] args) {
+        IMazeGenerator mg = new MyMazeGenerator();
+        Maze maze = mg.generate(111, 111);
+        SearchableMaze searchableMaze = new SearchableMaze(maze);
+        System.out.println("start path:" + maze.getStartPosition());
+        System.out.println("Goal path:" + maze.getGoalPosition());
+
+        solveProblem(searchableMaze, new BreadthFirstSearch());
+        solveProblem(searchableMaze, new DepthFirstSearch());
+        //solveProblem(searchableMaze, new BestFirstSearch());
+    }
+
+    private static void solveProblem(ISearchable domain, ISearchingAlgorithm searcher) {
+        //Solve a searching problem with a searcher
+        Solution solution = searcher.solve(domain);
+        System.out.println(String.format("'%s' algorithms.algorithm - nodes evaluated: %s", searcher.getName(), searcher.getNumberOfNodesEvaluated()));
+
+        //Printing Solution Path
+        System.out.println("Solution path:");
+        ArrayList<AState> solutionPath = solution.getSolutionPath();
+        for (int i = 0; i < solutionPath.size(); i++) {
+            System.out.println(String.format("%s. %s",i,solutionPath.get(i)));
+        }
+    }
+}
