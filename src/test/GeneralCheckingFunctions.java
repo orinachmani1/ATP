28b8b3e296e6c520f6fe2008a0462c6e90bb6b8e
orinachmani1 <73288898+orinachmani1@users.noreply.github.com>
2021-04-14 09:38:28 +0300

    I cleared the code - mostly deleting comments addtion of GeneralCheckingFunction class with out Github link

---
+++ src/test/GeneralCheckingFunctions.java
@@ -0,0 +1,10 @@
+package test;
+
+public class GeneralCheckingFunctions {
+
+    public static String getGithubLink(){
+        String githubLink = "https://github.com/orinachmani1/ATP";
+        return githubLink;
+    }
+
+}
--- src/test/RunMazeGenerator.java
+++ src/test/RunMazeGenerator.java
@@ -14,7 +14,7 @@ public class RunMazeGenerator {
         // prints the time it takes the algorithm to run
         System.out.println(String.format("Maze generation time(ms): %s", mazeGenerator.measureAlgorithmTimeMillis(100/*rows*/,100/*columns*/)));
         // generate another maze
-        Maze maze = mazeGenerator.generate(2/*rows*/, 2/*columns*/);
+        Maze maze = mazeGenerator.generate(5/*rows*/, 5/*columns*/);
 
         // prints the maze
         maze.print();
